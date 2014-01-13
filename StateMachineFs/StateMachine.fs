77b8918669547e4bf591a19bcfa7254a173f46b7
Patrick Kelly <patrick.kelly@sherwin.com>
2014-01-12 21:10:50 -0500

    Started state machine project. Initialize repo

---
+++ StateMachineFs/StateMachine.fs
@@ -0,0 +1,10 @@
+﻿namespace StateMachineFs
+
+type ContinueMessage = |Continue
+type Agent<'a> = MailboxProcessor<'a>
+
+type StateMachine () =
+    
+    member x.Run () = ()
+    member x.Continue () = ()
+
---
+++ StateMachineFsTests/StateMachineTests.fs
@@ -0,0 +1,21 @@
+﻿namespace StateMachineFsTests
+
+[<NUnit.Framework.TestFixture>]
+module StateMachineTests = 
+    open NUnit
+    open NUnit.Framework
+    open FsUnit
+    open StateMachineFs
+
+    [<Test>]
+    let ``it runs`` () =
+        let machine = new StateMachine()
+        machine.Run()
+        Assert.Pass()
+
+    [<Test>]
+    let ``it can be told to continue`` () =
+        let machine = new StateMachine()
+        machine.Run()
+        machine.Continue()
+        Assert.Pass()
